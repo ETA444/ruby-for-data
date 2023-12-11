408b7b69409e7dfb0bd52a50e2e977da7cc5b8d4
George <georgedreemer@proton.me>
2023-12-11 19:33:55 +0100

    Update dataview.rb

--- dataview/dataview.rb
+++ dataview/dataview.rb
@@ -48,9 +48,57 @@ end
 
 ## choosing the columns to work with
 puts "Available columns: #{csv_data.headers.join(', ')}".colorize(:yellow)
-puts "Note: DataView 1.0 only works with numeric columns."
-puts "Please enter the columns you want to work with, separated by commas:".colorize(:green)
-selected_columns = gets.chomp.split(',').map(&:strip)
+puts "------------------------"
+puts "Note: ".colorize(:light_blue)
+puts "(1) DataView 1.0 only works with clean data.".colorize(:light_blue)
+puts "(2) DataView 1.0 needs you to distinguish between column data type to generate appropriate visualizations and descriptive statistics.".colorize(:light_blue)
+puts "------------------------"
 
-### checking columns (need to be numeric)
+## get column names (*distinction between numerical and categorical columns)
+### numerical
+puts "Please enter the numerical column names, seperated by commas. (type 'none' to skip): ".colorize(:green)
+num_columns = gets.chomp.split(',').map(&:strip)
+
+if num_columns == 'none'
+	## categorical columns
+	puts "Please enter the categorical column names, seperated by commas: ".colorize(:green)
+	cat_columns = gets.chomp.split(',').map(&:strip)
+
+	if cat_columns == 'none'
+		puts "You can't skip both data types.".colorize(:red)
+	end
+else
+	puts "Please enter the categorical column names, seperated by commas. (type 'none' to skip): ".colorize(:green)
+	cat_columns = gets.chomp.split(',').map(&:strip)
+end
+
+# start handling the data #
+puts "Got it!".colorize(:light_blue)
+puts "Generating visualizations ...".colorize(:light_blue)
+
+## numerical
+if num_columns != 'none'
+	num_columns.each do |col|
+		data = csv_data[col]
+
+		# Histogram generation #
+		histogram = Rubyplot::Histogram.new(400)
+    	histogram.data data
+    	## Save visualization with following naming convention
+    	hist_fname = "#{col}-histogram-#{rand(100..999)}.png"
+    	puts "Saving histogram of the '#{col}' column, as: #{hist_fname}"
+    	histogram.write(hist_fname)
+		
+
+		# Boxplot generation #
+		# ..
+		## Save visualization with following naming convention
+  		
+
+  		# Descriptive statistics #
+  		# ..
+  		#
+end
+
+
 
